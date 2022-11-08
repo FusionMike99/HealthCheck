5925b2d8acc5814bea4f5296e07f0d24e2f088a8
Mykhailo Teziaiev <fusionmike99@gmail.com>
2022-11-08 23:42:12 +0200

    Add empty components: Home and FetchData

--- HealthCheck.UI/src/app/app.module.ts
+++ HealthCheck.UI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FetchDataComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
