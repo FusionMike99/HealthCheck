98d429f266afccc5d2849d011f7f460cbbd74242
Mykhailo Teziaiev <fusionmike99@gmail.com>
2022-11-09 00:02:45 +0200

    Implement navigation module and fetch data component

--- HealthCheck.UI/src/app/fetch-data/fetch-data.component.ts
+++ HealthCheck.UI/src/app/fetch-data/fetch-data.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-fetch-data',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fetch-data.component.css']
 })
 export class FetchDataComponent implements OnInit {
+  public forecasts?: WeatherForecast[];
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>(environment.baseUrl + 'api/weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
 }
